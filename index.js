a84f6216d73368276f5f99a4e8cf75581365cff5
Chris Searle <chris@chrissearle.org>
2017-11-05 07:50:20 +0100

    Add dash data

--- index.js
+++ index.js
@@ -21,6 +21,19 @@ const getStationsData = (err, devices) => {
     const now = Date.now() / 1000
 
     devices.forEach((device) => {
+        const deviceData = {
+            "station": device.station_name,
+            "wifi_status": device.wifi_status,
+            "noise": device.dashboard_data.Noise,
+            "temperature": device.dashboard_data.Temperature,
+            "humidity": device.dashboard_data.Humidity,
+            "pressure": device.dashboard_data.Pressure,
+            "CO2_level": device.dashboard_data.CO2,
+            "tags": [ 'netatmo', 'weather' ]
+        }
+
+        logger.info(`Netatmo station ${data.station} status`, deviceData)
+
         device.modules.forEach((component) => {
             const data = {
                 "module": component.module_name,
@@ -31,6 +44,22 @@ const getStationsData = (err, devices) => {
             }
 
             if (now - data.last_seen < (10*60)) {
+                switch (component.type) {
+                    case 'NAModule1':
+                        data.temperature = component.dashboard_data.Temperature
+                        data.humidity = component.dashboard_data.Humidity
+                        break
+                    case 'NAModule2':
+                        data.wind_angle = component.dashboard_data.WindAngle
+                        data.wind_strength = component.dashboard_data.WindStrength
+                        data.gust_angle = component.dashboard_data.GustAngle
+                        data.gust_strength= component.dashboard_data.GustStrength
+                        break
+                    case 'NAModule3':
+                        data.rain = component.dashboard_data.Rain
+                        break
+                }
+
                 logger.info(`Netatmo module ${data.module} status`, data)
             }
         })
